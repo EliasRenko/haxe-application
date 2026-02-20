ec99b4d3df3be26ee29c5c089eae3c52847ad8ec
Fedorenko Elias <FedorenkoElias@yandex.ru>
2026-02-20 16:32:46 +0200

    Fix color channel assignments in Color class for correct RGBA representation

--- src/utils/Color.hx
+++ src/utils/Color.hx
@@ -17,21 +17,21 @@ class Color {
 	public function new(value:Int) {
 		this.hexValue = value;
 	}
-	
+
 	private function __setAlpha():Void {
-		a = ((__hexValue >> 24) & 0xff) / 255.0;
+		a = (__hexValue & 0xff) / 255.0;
 	}
 	
 	private function __setBlue():Void {
-		b = (__hexValue & 0xff) / 255.0;
+		b = ((__hexValue >> 8) & 0xff) / 255.0;
 	}
 	
 	private function __setGreen():Void {
-		g = ((__hexValue >> 8) & 0xff) / 255.0;
+		g = ((__hexValue >> 16) & 0xff) / 255.0;
 	}
 	
 	private function __setRed():Void {
-		r = ((__hexValue >> 16) & 0xff) / 255.0;
+		r = ((__hexValue >> 24) & 0xff) / 255.0;
 	}
 	
 	// Getters and setters
