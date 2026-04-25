895e15deb4621e8e8002a0e5cd97c3e146c7a3e0
Fedorenko Elias <FedorenkoElias@yandex.ru>
2026-04-25 21:24:19 +0300

    web resources

---
+++ res/web/main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      webSecurity: false,
+      devTools: true
+    }
+  })
+
+  win.webContents.openDevTools()
+
+  win.loadFile('editor.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
